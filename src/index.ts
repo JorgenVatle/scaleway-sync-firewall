476f2ed3e52a570067e5db3e61c1327c18916053
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-06 12:38:19 +0300

    Add group syncing script to package index.

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import SecurityGroup from './SecurityGroup';
+import { ScalewayZone } from './library/interfaces/Scaleway';
+
+/**
+ * Validate that all required environment variables are set.
+ */
+function validateEnvironmentVariables() {
+    const requiredEnv = ['AUTH_TOKEN', 'MASTER_ID', 'MASTER_ZONE', 'TARGET_GROUPS_JSON'];
+
+    requiredEnv.forEach((variable) => {
+        if (!process.env[variable]) {
+            throw Error(`Missing environment variable! See .env.defaults [Missing: ${variable}]`)
+        }
+    });
+}
+
+function targets(): Promise<SecurityGroup>[] {
+    const targets = require('../targets.json');
+    return targets.map((target: { id: string, zone: ScalewayZone }) => {
+        return SecurityGroup.get(target.zone, target.id);
+    });
+}
+
+/**
+ * Current master group.
+ */
+async function syncGroups() {
+    const primaryGroup = await SecurityGroup.get(<ScalewayZone>process.env.MASTER_ZONE, process.env.MASTER_ID!);
+    const primaryRules = await primaryGroup.rules;
+    const targetGroups = await Promise.all(targets());
+
+    await Promise.all(targetGroups.map(async (group) => {
+        await group.clearRules();
+        await group.addRules(primaryRules);
+    }))
+}
+
+validateEnvironmentVariables();
+syncGroups().then(() => {
+    console.info('Successfully synchronized groups!');
+}).catch((error) => {
+    console.error('Encountered error syncing groups!');
+    console.error(error);
+});
