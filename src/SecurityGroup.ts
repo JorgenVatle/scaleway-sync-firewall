1262a59c978b9d57f579b44367b970417b8c5f1f
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-06 10:31:51 +0300

    Add rules() getter to SecurityGroup.

--- src/SecurityGroup.ts
+++ src/SecurityGroup.ts
@@ -1,8 +1,15 @@
 import PrepareModel from './library/Model';
 import SecurityGroupInterface from './library/interfaces/SecurityGroupInterface';
+import FirewallRule from './FirewallRule';
 
 export default class SecurityGroup extends PrepareModel<SecurityGroupInterface>('/security_groups') {
 
-
+    /**
+     * A security group has many firewall rules.
+     */
+    public get rules(): Promise<FirewallRule[]> {
+        // @ts-ignore
+        return this.hasMany(FirewallRule, this.zone, `${this.entry.id}/rules`, 'rules');
+    }
 
 }
