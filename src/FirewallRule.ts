6eb3ec8d405e2ad110243c2ec1423e40998fa0b2
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-06 07:53:59 +0300

    Refactor to accommodate for Model generic.

--- src/FirewallRule.ts
+++ src/FirewallRule.ts
@@ -1,5 +1,7 @@
 import Model from './Model';
+import FirewallRuleInterface from './interfaces/FirewallRuleInterface';
 
-export default class FirewallRule extends Model {
+export default class FirewallRule extends Model<FirewallRuleInterface> {
 
 }
+
--- src/SecurityGroup.ts
+++ src/SecurityGroup.ts
@@ -1,7 +1,7 @@
 import Model from './Model';
+import SecurityGroupInterface from './interfaces/SecurityGroupInterface';
 
-export default class SecurityGroup extends Model {
-
+export default class SecurityGroup extends Model<SecurityGroupInterface> {
 
 
 }
