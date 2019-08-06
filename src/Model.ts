d65d77849a2bab59dbf606e419ce2f8bdfb3f97f
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-06 07:52:17 +0300

    Make Model class a generic.

--- src/Model.ts
+++ src/Model.ts
@@ -1,3 +1,15 @@
-export default abstract class Model {
+export default abstract class Model<T> {
+
+    /**
+     * Data entry.
+     */
+    entry: T;
+
+    /**
+     * Model constructor.
+     */
+    constructor(entry: T) {
+        this.entry = entry;
+    }
 
 }
