849d21243d0d71b4301dccc10400c4d0ba0fb25a
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-06 10:48:37 +0300

    Add delete() method to FirewallRule model.

--- src/FirewallRule.ts
+++ src/FirewallRule.ts
@@ -17,4 +17,11 @@ export default class FirewallRule extends PrepareModel<FirewallRuleInterface>('/
         return `${this.meta.securityGroupId}/rules/${this.entry.id}`;
     }
 
+    /**
+     * Delete the current firewall rule.
+     */
+    public delete() {
+        return this.client.delete('/');
+    }
+
 }
