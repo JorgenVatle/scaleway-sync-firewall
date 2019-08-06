56da46d91ff448ae0c8683efae48cbf18ea70652
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-06 10:44:40 +0300

    Add path() getter to FirewallRule.

--- src/FirewallRule.ts
+++ src/FirewallRule.ts
@@ -3,5 +3,18 @@ import FirewallRuleInterface from './library/interfaces/FirewallRuleInterface';
 
 export default class FirewallRule extends PrepareModel<FirewallRuleInterface>('/security_groups') {
 
+    /**
+     * Firewall rule metadata.
+     */
+    protected meta!: {
+        securityGroupId: string;
+    };
+
+    /**
+     * Rule path.
+     */
+    protected get path() {
+        return `${this.meta.securityGroupId}/rules/${this.entry.id}`;
+    }
 
 }
